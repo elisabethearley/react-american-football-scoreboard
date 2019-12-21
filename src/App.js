61256804d8f2275109aee17f14661d3b14da19d7
elisabethearley <elisabethearley@gmail.com>
2019-12-21 11:06:18 -0500

    changed styling

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name">Wolves</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
@@ -33,7 +33,7 @@ function App() {
           </div>
           <div className="timer">{minutes}{seconds}</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
+            <h2 className="away__name">Birds</h2>
             <div className="away__score">{awayValue}</div>
           </div>
         </div>
